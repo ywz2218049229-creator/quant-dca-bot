b09c296f201df8655d3a942508a4d71f17015d2a
ywz2218049229-creator <ywz2218049229@gmail.com>
2026-06-11 17:35:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Hello Quant Bot!")
+import os
+import requests
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+message = """
+🎉 恭喜！
+
+GitHub 已成功连接 Telegram。
+
+这是你的第一条自动推送消息。
+"""
+
+requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+)
+
+print("Telegram message sent")
