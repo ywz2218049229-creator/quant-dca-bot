698678757179533b4c052c7fad903f5a03c9f5d8
ywz2218049229-creator <ywz2218049229@gmail.com>
2026-06-15 10:28:25 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,48 @@
+# ==========================
+# 资产配置
+# ==========================
+
 CURRENT_NDX = 3855
 CURRENT_SPX = 1728
 
 CASH = 117577
 
-MONTHLY_INVEST = 1500
+# ==========================
+# 年度预算
+# ==========================
+
+MIN_BUDGET = 25000
+TARGET_BUDGET = 30000
+MAX_BUDGET = 35000
+
+START_DATE = "2026-06-15"
+END_DATE = "2027-06-15"
+
+# ==========================
+# 目标配置
+# ==========================
 
 TARGET_NDX = 0.70
 TARGET_SPX = 0.30
+
+# ==========================
+# 平滑系数
+# ==========================
+
+YESTERDAY_WEIGHT = 0.7
+TODAY_WEIGHT = 0.3
+
+# ==========================
+# 权重股
+# ==========================
+
+MEGA_CAPS = {
+    "AAPL": "Apple",
+    "MSFT": "Microsoft",
+    "NVDA": "NVIDIA",
+    "AMZN": "Amazon",
+    "GOOGL": "Google",
+    "META": "Meta",
+    "AVGO": "Broadcom",
+    "TSLA": "Tesla"
+}
